168104b31d05764010eaedf095150b26956348df
Al Davidson <apdavidson@gmail.com>
2017-05-30 14:11:55 +0100

    add new_item action and working hashable concern

---
+++ app/models/concerns/hashable.rb
@@ -0,0 +1,43 @@
+require 'active_support/concern'
+
+module Concerns
+  # automatically does the following:
+  # 1) records a hash of the embedding class's attributes
+  #    (minus the mongo _id) in item_hash
+  # 2) ensures that the hash is unique
+  #
+  # You can provide a class to implement the digest in the config var
+  # Rails.configuration.x.hash_type (default is Digest::SHA256)
+  module Hashable
+    extend ActiveSupport::Concern
+    include Mongoid::Document
+    
+    included do
+      include Mongoid::Slug
+
+      field :item_hash, type: String
+      slug :item_hash
+
+      before_validation { |document| set_hash(:item_hash, document) }
+      validates :item_hash, uniqueness: true, presence: true
+
+      def set_hash(field_name, document)
+        send("#{field_name}=", formatted_hash(document))
+      end
+
+      def formatted_hash(document)
+        hash_type = hash_type_or_default
+        
+        doc_as_json = document.attributes.except('_id').to_h.to_s
+        type_specifier = type.to_s.split('::').last.to_s.downcase
+
+        [type_specifier, hash_type.hexdigest(doc_as_json)].join(':')
+      end
+
+      def hash_type_or_default
+        type = Rails.configuration.x.hash_type
+        type.present? ? type : Digest::SHA256
+      end
+    end
+  end
+end
